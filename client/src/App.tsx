ea0bcaa84d50e943f760b5bcf19fdb01e011b045
Mohd Zhafri <mohdzhafri.z@maybank.com>
2021-08-30 10:46:32 +0800

    improvement: UX improvement.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Container from "@material-ui/core/Container";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import AppBar from "@material-ui/core/AppBar";
 import Slider from "@material-ui/core/Slider";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const containerStyle = {
-  width: "100vh",
+  width: "100%",
   height: "600px",
 };
 
@@ -22,6 +25,29 @@ interface TaxiData {
   driver_id: string;
   location: PositionData;
 }
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      marginRight: 10,
+      marginLeft: 10,
+      marginTop: 10,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+      overflow: "hidden",
+    },
+    taxiCountGutter: {
+      height: 20,
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
 
 function App() {
   const [position, setPosition] = useState<PositionData>({
@@ -69,7 +95,7 @@ function App() {
     let intervalID: any;
     if (position?.latitude && position?.longitude) {
       setTaxisPosition();
-      intervalID = setInterval(setTaxisPosition, 10000);//TODO: interval time should be in env config
+      intervalID = setInterval(setTaxisPosition, 10000); //TODO: interval time should be in env config
     }
 
     return () => {
@@ -113,69 +139,108 @@ function App() {
     googleMapsApiKey: "AIzaSyBWerOUyFloxkqH9kPk5o18uOn_F4KbuCg", //TODO: Use env config
   });
 
-  return isLoaded ? (
+  const classes = useStyles();
+
+  return (
     <>
-      <CssBaseline />
-      <Container fixed>
-        <AppBar position="static">
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={changeLocationToSingapore}
-          >
-            Singapore
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={changeLocationToLondon}
-          >
-            London
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={changeLocationToCurrent}
-          >
-            Current
-          </Button>
-          <Slider
-            defaultValue={10}
-            getAriaValueText={valueText}
-            aria-labelledby="discrete-slider"
-            valueLabelDisplay="auto"
-            step={10}
-            marks
-            min={10}
-            max={50}
-            color="secondary"
-            onChange={updateTaxiCount}
-          />
-        </AppBar>
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={{
-            lat: position.latitude,
-            lng: position.longitude,
-          }}
-          zoom={15}
-        >
-          <>
-            {taxis?.map((driver: TaxiData) => (
-              <Marker
-                key={driver.driver_id}
-                position={{
-                  lat: driver.location.latitude,
-                  lng: driver.location.longitude,
+    <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            The Taxi
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>
+            <Grid container spacing={1} direction="column">
+              <Grid item xs={12}>
+                <Typography id="discrete-slider" gutterBottom>
+                  Locations
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  onClick={changeLocationToSingapore}
+                  fullWidth
+                >
+                  Singapore
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={changeLocationToLondon}
+                  fullWidth
+                >
+                  London
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={changeLocationToCurrent}
+                  fullWidth
+                >
+                  Current
+                </Button>
+              </Grid>
+            </Grid>
+            <div className={classes.taxiCountGutter} />
+            <Grid container>
+              <Grid xs={12} alignContent="flex-start">
+                <Typography id="discrete-slider" gutterBottom>
+                  Taxi Count
+                </Typography>
+                <Slider
+                  defaultValue={10}
+                  getAriaValueText={valueText}
+                  aria-labelledby="discrete-slider"
+                  valueLabelDisplay="auto"
+                  step={10}
+                  marks
+                  min={10}
+                  max={50}
+                  color="secondary"
+                  onChange={updateTaxiCount}
+                />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={10}>
+          <Paper className={classes.paper}>
+            {isLoaded && (
+              <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={{
+                  lat: position.latitude,
+                  lng: position.longitude,
                 }}
-              />
-            ))}
-          </>
-        </GoogleMap>
-      </Container>
+                zoom={15}
+              >
+                <>
+                  {taxis?.map((driver: TaxiData) => (
+                    <Marker
+                      key={driver.driver_id}
+                      position={{
+                        lat: driver.location.latitude,
+                        lng: driver.location.longitude,
+                      }}
+                    />
+                  ))}
+                </>
+              </GoogleMap>
+            )}
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
     </>
-  ) : (
-    <></>
   );
 }
 
